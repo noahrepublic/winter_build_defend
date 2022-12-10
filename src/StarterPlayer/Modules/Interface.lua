f79ca885aef44389b8873dc34b0f57c636792be0
Jack Chambers <jtcroxley@gmail.com>
2022-12-09 16:30:11 -0800

    Placeholder Components

---
+++ src/StarterPlayer/Modules/Interface.lua
@@ -0,0 +1,28 @@
+-- @title: Interface.lua
+-- @author: qweekertom
+-- @date: 2022-12-09
+
+-- placeholder to keep the folder alive
+
+--> Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+--> Loader, Modules, and Util
+local loader = require(ReplicatedStorage.Loader)
+
+--> Module Definition
+local module = {}
+local SETTINGS = {}
+
+--> Variables
+
+--> Private Functions
+
+--> Module Functions
+
+--> Loader Methods
+function module.Start()
+	print("Hello world!")
+end
+
+return module
