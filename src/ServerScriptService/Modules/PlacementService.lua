53f2ef06468ad58914f994903b7379d4aca3dc1a
Noah <noahdevelopsthings@gmail.com>
2022-12-11 16:22:09 -0400

    Added for ss

---
+++ src/ServerScriptService/Modules/PlacementService.lua
@@ -0,0 +1,24 @@
+-- @title: PlacementService.lua
+-- @author: noahrepublic
+-- @date: 2022-12-11
+
+--> Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+--> Loader, Modules, and Util
+local loader = require(ReplicatedStorage.Loader)
+
+--> Module Definition
+local module = {}
+local SETTINGS = {}
+
+--> Variables
+
+--> Private Functions
+
+--> Module Functions
+
+--> Loader Methods
+function module.Start() end
+
+return module
