fdd01771389a843d0beb99bb10cd28bced7f6774
Noah <noahdevelopsthings@gmail.com>
2022-12-10 20:32:13 -0400

    File structure + loader compatibility

--- src/ServerScriptService/Modules/PlayerRegistry/init.lua
+++ src/ServerScriptService/Modules/PlayerRegistry/init.lua
@@ -1,32 +1,67 @@
---[[
-	Stores custom Player Objects from the "Player" class
-]]
+-- @title: PlayerRegistry.lua
+-- @author: noahrepublic
+-- @date: 2022-12-10
 
-local Player = require(script:FindFirstChild("Player"))
+--> Services
+local PlayerService = game:GetService("Players")
+--local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local Players = {} -- Stores players
+--> Loader, Modules, and Util
+--local loader = require(ReplicatedStorage.Loader)
+local Player = require(script.Player)
 
+--> Module Definition
 local PlayerRegistry = {}
 PlayerRegistry.__index = PlayerRegistry
--- Functions --
+
+--> Variables
+
+local Players = {}
+
+--> Private Functions
+
+--> Module Functions
 
 function PlayerRegistry.AddPlayer(plr: Player)
+	assert(plr ~= nil, "Passed player value is nil!")
+
+	if Players[plr] ~= nil then
+		return Players[plr]
+	end
+
 	local player_class = Player.new(plr)
 	Players[plr] = player_class
 	return player_class
 end
 
 function PlayerRegistry.GetPlayer(plr: Player)
+	assert(plr ~= nil, "Passed player value is nil!")
+
+	if Players[plr] == nil then
+		return PlayerRegistry.AddPlayer(plr)
+	end
 	return Players[plr]
 end
 
 function PlayerRegistry.RemovePlayer(plr: Player)
+	assert(plr ~= nil, "Passed player value is nil!")
+
 	local player_class = PlayerRegistry.GetPlayer(plr)
-	assert(player_class ~= nil, "Passed player value is nil!")
 	Players[plr] = nil
 	player_class:Disconnect()
 end
 
 PlayerRegistry.Disconnect = PlayerRegistry.RemovePlayer
+
+--> Loader Methods
+function PlayerRegistry.Start()
+	PlayerService.PlayerAdded:Connect(function(plr)
+		PlayerRegistry.AddPlayer(plr)
+	end)
+
+	PlayerService.PlayerRemoving:Connect(function(plr)
+		PlayerRegistry.RemovePlayer(plr)
+	end)
+end
 
 return PlayerRegistry
